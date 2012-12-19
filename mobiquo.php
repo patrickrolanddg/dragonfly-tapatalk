1b661746706c994d931bb4929840a5607e7231e8
nanocaiordo <nanocaiordo@localhost>
2012-12-19 16:13:37 +1100

    Using MOBPATH.

--- mobiquo.php
+++ mobiquo.php
@@ -12,34 +12,22 @@ define('IN_MOBIQUO', true);
 
 // Set up Debug File
 $myFile = "log.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
-
-// Initialise DragonflyCMS core, security, session, and user config
-define('XMLFEED', 1);
-require_once('../includes/cmsinit.inc');
-require_once('../includes/classes/security.php');
-require_once('../includes/classes/session.php');
-require_once('../includes/classes/cpg_member.php');
-$CPG_SESS = array();
-$SESS = new cpg_session();
-$CLASS['member'] = new cpg_member();
-$userinfo = $_SESSION['CPG_USER'];
+$fh = fopen($myFile, 'w') or die("can't open file"); // REALLY BAD
 
 // Initialise CPG-BB (formally phpbb)
 define('IN_PHPBB', true);
 global $phpbb_root_path;
-$phpbb_root_path = '../modules/Forums/';
-$webrootpath = '../';
-set_include_path(get_include_path() . PATH_SEPARATOR . $webrootpath);
-include($phpbb_root_path.'common.php');
+$phpbb_root_path = BASEDIR.'modules/Forums/';
 
+include($phpbb_root_path.'common.php');
+define('MOBPATH', BASEDIR."modules/$module_name/");
 // Initialise tapatalk
-include("./include/xmlrpc.inc");
-include("./include/xmlrpcs.inc");
-require('./config/config.php');
-require('./error_code.php');
-require('./mobiquo_common.php');
-require('./server_define.php');
+include(MOBPATH.'include/xmlrpc.inc');
+include(MOBPATH.'include/xmlrpcs.inc');
+require(MOBPATH.'config/config.php');
+require(MOBPATH.'error_code.php');
+require(MOBPATH.'mobiquo_common.php');
+require(MOBPATH.'server_define.php');
 $mobiquo_config = get_mobiquo_config();
 $phpEx = $mobiquo_config['php_extension'];
 
