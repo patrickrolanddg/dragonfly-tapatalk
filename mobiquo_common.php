aa4490173a10c387f33084d4d4119e27285637c0
hybiepoo <hybiepoo@localhost>
2012-12-22 18:17:34 +1100

    Added get_thread.php from phpbb3 module. Looks like we're missing some functions (sql_build_query) for example. Might be easier to build this function than rewriting from scratch?

--- mobiquo_common.php
+++ mobiquo_common.php
@@ -434,3 +434,82 @@ function get_preg_expression($mode)
 
       return '';
   }
+
+// Added by hybrid to possibly simplify the conversion from phpbb3 plugin
+
+
+function request_var($var_name, $default, $multibyte = false, $cookie = false)
+   {
+       if (!$cookie && isset($_COOKIE[$var_name]))
+       {
+           if (!isset($_GET[$var_name]) && !isset($_POST[$var_name]))
+           {
+               return (is_array($default)) ? array() : $default;
+           }
+           $_REQUEST[$var_name] = isset($_POST[$var_name]) ? $_POST[$var_name] : $_GET[$var_name];
+       }
+   
+       $super_global = ($cookie) ? '_COOKIE' : '_REQUEST';
+       if (!isset($GLOBALS[$super_global][$var_name]) || is_array($GLOBALS[$super_global][$var_name]) != is_array($default))
+       {
+           return (is_array($default)) ? array() : $default;
+       }
+   
+       $var = $GLOBALS[$super_global][$var_name];
+       if (!is_array($default))
+       {
+           $type = gettype($default);
+       }
+       else
+       {
+           list($key_type, $type) = each($default);
+           $type = gettype($type);
+           $key_type = gettype($key_type);
+           if ($type == 'array')
+           {
+               reset($default);
+               $default = current($default);
+               list($sub_key_type, $sub_type) = each($default);
+               $sub_type = gettype($sub_type);
+               $sub_type = ($sub_type == 'array') ? 'NULL' : $sub_type;
+               $sub_key_type = gettype($sub_key_type);
+           }
+       }
+  
+      if (is_array($var))
+      {
+          $_var = $var;
+          $var = array();
+  
+          foreach ($_var as $k => $v)
+          {
+              set_var($k, $k, $key_type);
+              if ($type == 'array' && is_array($v))
+              {
+                  foreach ($v as $_k => $_v)
+                  {
+                      if (is_array($_v))
+                      {
+                          $_v = null;
+                      }
+                      set_var($_k, $_k, $sub_key_type, $multibyte);
+                      set_var($var[$k][$_k], $_v, $sub_type, $multibyte);
+                  }
+              }
+              else
+              {
+                  if ($type == 'array' || is_array($v))
+                  {
+                      $v = null;
+                  }
+                  set_var($var[$k], $v, $type, $multibyte);
+              }
+          }
+      }
+      else
+      {
+          set_var($var, $var, $type, $multibyte);
+      }
+  
+      return $var;
+  }
