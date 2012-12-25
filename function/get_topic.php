7fe26ed6e0acd6b2150a29917921e4170b3a9364
nanocaiordo <nanocaiordo@localhost>
2012-12-25 22:20:46 +1100

    Improved pagination support.

--- function/get_topic.php
+++ function/get_topic.php
@@ -16,25 +16,47 @@ function get_topic_func($xmlrpc_params)
 	$forum_id = isset($params[0]) ? intval($params[0]) : 0;
 	if (!$forum_id) return get_error(3);
 
-	$start_num  = isset($params[1]) && is_int($params[1]) ? $params[1] : 0;
-	$end_num    = isset($params[2]) && is_int($params[2]) ? $params[2] : 19;
-	$topic_type = !isset($params[3]) ? false : $params[3] == 'TOP' ? '1' : ($params[3] == 'ANN' ? '2' : false);
-	$topicQString = $topic_type ?: '0,1,2';
+	$start_num = 1;
+	$end_num = 20;
+	$topic_type = '1,2,3';
+
+	switch (count($params))
+	{
+		case 4:
+			$topic_type = $params[3] == 'TOP' ? '1' : ($params[3] == 'ANN' ? '2' : $topic_type);
+			$end_num    = intval($params[2]);
+			$start_num  = intval($params[1]);
+			break;
+		case 3:
+			$end_num    = intval($params[2]);
+			$start_num  = intval($params[1]);
+			break
+		case 2:
+			$topic_type = $params[2] == 'TOP' ? '1' : ($params[2] == 'ANN' ? '2' : $topic_type);
+	}
 
 	# check if topic index is out of range
 	if ($start_num > $end_num) return get_error(5);
 	# return at most 50 topics
-	if ($end_num - $start_num >= 50) $end_num = $start_num + 49;
+	if ($end_num - $start_num >= 50) $end_num = $start_num + 50;
+	# if both are present and are set to 0, return the first topic only
+	if (0 === $start_num 0 === $end_num) {
+		$start_num = 1;
+		$end_num = 1;
+	}
 
 	global $db, $prefix;
 
 	$forum = mobi_forums($forum_id);
 	$forum = !empty($forum[0]) ? $forum[0] : 0;
 	if (!$forum) return get_error(3);
+
+	if (!$forum(['auth_read'])) get_error(7);
+
 	if (isset($forum['subforums'])) unset($forum['subforums']);
 	if (FORUM_LINK == $forum_data['forum_type']) return get_error(4);
 
-	$sql_limit = $end_num - $start_num + 1;  // num of topics needs to be return, default is 20, at most 50
+	$sql_limit = $end_num - $start_num + 1;
 
 	$result = $db->sql_query("SELECT t.*, u.username, u.user_avatar, u.user_avatar_type, u.user_allowavatar
 		FROM {$prefix}_bbtopics t
@@ -46,8 +68,13 @@ function get_topic_func($xmlrpc_params)
 		LIMIT $sql_limit OFFSET $start_num");
 
 	$topic_list = array();
+	$unread_sticky_num = $unread_announce_count = 0;
 	while ($row = $db->sql_fetchrow($result, SQL_ASSOC))
 	{
+		if (is_user()) {
+			if (POST_ANNOUNCE == $row['type']) ++$unread_announce_count;
+			if (POST_STICKY   == $row['type']) ++$unread_sticky_num;
+		}
 		$short_content = get_short_content($row['topic_first_post_id']);
 		$user_avatar_url = $row['user_allowavatar'] ? get_user_avatar_url($row['user_avatar'], $row['user_avatar_type']) : '';
 
@@ -58,7 +85,7 @@ function get_topic_func($xmlrpc_params)
 			'topic_author_id'   => new xmlrpcval($row['topic_poster']),
 			'topic_author_name' => new xmlrpcval(html_entity_decode($row['username']), 'base64'),
 			'is_subscribed'     => new xmlrpcval(!empty($_SESSION['CPG_SESS']['Forums']['track_topics'][$row['topic_id']]), 'boolean'),
-			'can_subscribe'     => new xmlrpcval(is_user(), 'boolean'),
+			'can_subscribe'     => new xmlrpcval($forum['auth_read'], 'boolean'),
 			'is_closed'         => new xmlrpcval(TOPIC_LOCKED == $row['topic_status'], 'boolean'),
 			'last_reply_time'   => new xmlrpcval(mobiquo_iso8601_encode($row['topic_time']), 'dateTime.iso8601'),
 			'reply_number'      => new xmlrpcval($row['topic_replies'], 'int'),
@@ -71,18 +98,18 @@ function get_topic_func($xmlrpc_params)
 		$topic_list[] = new xmlrpcval($rpc, 'struct');
 	}
 	$db->sql_freeresult($result);
+	if (empty($topic_list)) $topic_list = null;
 
-	$unread_sticky_num = $unread_announce_count = 0;
 	$response = new xmlrpcval(array(
 		'total_topic_num' => new xmlrpcval($forum['forum_topics'], 'int'),
 		'forum_id'        => new xmlrpcval($forum['forum_id'], 'string'),
 		'forum_name'      => new xmlrpcval(html_entity_decode($forum['forum_name']), 'base64'),
-		'can_post'        => new xmlrpcval(is_user(), 'boolean'), // needs to check forum auths
+		'can_post'        => new xmlrpcval($forum['auth_post'], 'boolean'), // needs to check forum auths
 		'unread_sticky_count'   => new xmlrpcval($unread_sticky_num, 'int'),
 		'unread_announce_count' => new xmlrpcval($unread_announce_count, 'int'),
-		//'prefixes'        => new xmlrpcval(array(), 'array'),
-		//'prefix_id'        => new xmlrpcval('string'),
-		//'prefix_display_name'        => new xmlrpcval('', 'base64'),
+		'can_subscribe'   => new xmlrpcval($forum['auth_read'], 'boolean'),
+		'is_subscribed'   => new xmlrpcval(!empty($_SESSION['CPG_SESS']['Forums']['track_forums'][$forum['forum_id']]), 'boolean'),
+		'prefixes'        => new xmlrpcval(array(), 'array'),
 		'topics'          => new xmlrpcval($topic_list, 'array'),
 		), 'struct');
 
