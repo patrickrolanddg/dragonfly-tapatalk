720422022b0694fd248ffb332672a39f1489b069
nanocaiordo <nanocaiordo@localhost>
2012-12-25 22:19:45 +1100

    Using full paths.

--- mobiquo.php
+++ mobiquo.php
@@ -12,13 +12,15 @@ define('IN_MOBIQUO', true);
 
 $module_name = 'Forums';
 
-// Initialise DragonflyCMS
+// Initialise DragonflyCMS framework
 require_once('../includes/cmsinit.inc');
 
 if (!is_active('Forums')) {
 	header("{$_SERVER['SERVER_PROTOCOL']} 503 Service Unavailable");
 	exit;
 }
+
+define('MOBIDIR', BASEDIR . basename(dirname($_SERVER['SCRIPT_NAME'])).'/');
 
 if (!defined('BASEHREF')) define('BASEHREF', $BASEHREF);
 define('MOBIHREF', BASEHREF . basename(dirname($_SERVER['SCRIPT_NAME'])).'/');
@@ -29,31 +31,32 @@ set_include_path(get_include_path() . PATH_SEPARATOR . BASEDIR);
 include(BASEDIR.'modules/Forums/nukebb.php');
 
 // Initialise tapatalk
-include("./include/xmlrpc.inc");
-include("./include/xmlrpcs.inc");
-require('./config/config.php');
-require('./error_code.php');
-require('./mobiquo_common.php');
-require('./server_define.php');
-
+include(MOBIDIR.'include/xmlrpc.inc');
+include(MOBIDIR.'include/xmlrpcs.inc');
+require(MOBIDIR.'error_code.php');
+require(MOBIDIR.'config/config.php');
 
 $mobiquo_config = get_mobiquo_config();
-
 if (!$mobiquo_config['is_open']) {
 	header("{$_SERVER['SERVER_PROTOCOL']} 503 Service Unavailable");
 	exit;
 }
+
+require(MOBIDIR.'mobiquo_common.php');
+require(MOBIDIR.'server_define.php');
 
 $phpEx = $mobiquo_config['php_extension'];
 $xmlrpc_internalencoding = 'UTF-8';
 $xmlrpcName = 'DragonflyCMS/'.CPG_NUKE.' (PHP '.PHP_MAJOR_VERSION.'; '.PHP_SAPI.'; '.PHP_OS.') XML-RPC Tapatalk/3';
 $xmlrpcVersion = 'v'.$mobiquo_config['sys_version'];
 
+header('Mobiquo_is_login:'.(is_user() ? 'true' : 'false'));
+
 // Get requested function and load file
 $request_method_name = get_method_name();
 if ($request_method_name && isset($server_param[$request_method_name]))
 {
-    require('./function/'.$request_method_name.'.php');
+	require(MOBIDIR.'function/'.$request_method_name.'.php');
 }
 
 $rpcServer = new xmlrpc_server($server_param, false);
