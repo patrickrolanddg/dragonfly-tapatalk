7f97ec4001556ef1ecdc1e923e609a3ee892deeb
nanocaiordo <nanocaiordo@localhost>
2012-12-26 14:30:16 +1100

    Improvements and fixed pagination.

--- function/get_topic.php
+++ function/get_topic.php
@@ -1,8 +1,10 @@
 <?php
-/**
-*
-*
-*/
+/*======================================================================*\
+|| #################################################################### ||
+|| # DragonflyCMS Tapatalk plugin                                     # ||
+|| # Written by Jeff Mills (hybiepoo@hotmail.com)                     # ||
+|| #################################################################### ||
+\*======================================================================*/
 
 # 0 POST_NORMAL
 # 1 POST_STICKY
@@ -16,33 +18,39 @@ function get_topic_func($xmlrpc_params)
 	$forum_id = isset($params[0]) ? intval($params[0]) : 0;
 	if (!$forum_id) return get_error(3);
 
-	$start_num = 1;
-	$end_num = 20;
-	$topic_type = '1,2,3';
+	$offset = 0;
+	$limit = 20;
+	$topic_type = '0,1,2';
 
 	switch (count($params))
 	{
 		case 4:
+			$offset = intval($params[1]); // start_num
+			$limit  = intval($params[2]); // end_num
 			$topic_type = $params[3] == 'TOP' ? '1' : ($params[3] == 'ANN' ? '2' : $topic_type);
-			$end_num    = intval($params[2]);
-			$start_num  = intval($params[1]);
 			break;
 		case 3:
-			$end_num    = intval($params[2]);
-			$start_num  = intval($params[1]);
+			$offset = intval($params[1]);
+			$limit  = intval($params[2]);
 			break;
 		case 2:
-			$topic_type = $params[2] == 'TOP' ? '1' : ($params[2] == 'ANN' ? '2' : $topic_type);
+			$topic_type = $params[1] == 'TOP' ? '1' : ($params[1] == 'ANN' ? '2' : $topic_type);
 	}
 
-	# check if topic index is out of range
-	if ($start_num > $end_num) return get_error(5);
-	# return at most 50 topics
-	if ($end_num - $start_num >= 50) $end_num = $start_num + 50;
-	# if both are present and are set to 0, return the first topic only
-	if (0 === $start_num && 0 === $end_num) {
-		$start_num = 1;
-		$end_num = 1;
+	if ($offset > $limit) {
+		# check if topic index is out of range
+		return get_error(5);
+	}
+	else if ($limit - $offset >= 50) {
+		# return at most 50 topics
+		$limit = 50;
+	}
+	else if (0 === $offset && 0 === $limit) {
+		# if both are present and are set to 0, return the first topic only
+		$limit = 1;
+	}
+	else if ($offset || $limit) {
+		$limit = 1 + $limit - $offset;
 	}
 
 	global $db, $prefix;
@@ -54,9 +62,7 @@ function get_topic_func($xmlrpc_params)
 	if (!$forum['auth_read']) get_error(7);
 
 	if (isset($forum['subforums'])) unset($forum['subforums']);
-	if (FORUM_LINK == $forum_data['forum_type']) return get_error(4);
-
-	$sql_limit = $end_num - $start_num + 1;
+	if (FORUM_LINK == $forum['forum_type']) return get_error(4);
 
 	$result = $db->sql_query("SELECT t.*, u.username, u.user_avatar, u.user_avatar_type, u.user_allowavatar
 		FROM {$prefix}_bbtopics t
@@ -65,7 +71,7 @@ function get_topic_func($xmlrpc_params)
 		WHERE t.forum_id = $forum_id
 		AND t.topic_type IN ($topic_type)
 		ORDER BY t.topic_id DESC
-		LIMIT $sql_limit OFFSET $start_num");
+		LIMIT $limit OFFSET $offset");
 
 	$topic_list = array();
 	$unread_sticky_num = $unread_announce_count = 0;
@@ -84,7 +90,7 @@ function get_topic_func($xmlrpc_params)
 			'topic_title'       => new xmlrpcval(html_entity_decode(strip_tags($row['topic_title']), ENT_QUOTES, 'UTF-8'), 'base64'),
 			'topic_author_id'   => new xmlrpcval($row['topic_poster']),
 			'topic_author_name' => new xmlrpcval(html_entity_decode($row['username']), 'base64'),
-			'is_subscribed'     => new xmlrpcval(!empty($_SESSION['CPG_SESS']['Forums']['track_topics'][$row['topic_id']]), 'boolean'),
+			'is_subscribed'     => new xmlrpcval($forum['auth_read'] && !empty($_SESSION['CPG_SESS']['Forums']['track_topics'][$row['topic_id']]), 'boolean'),
 			'can_subscribe'     => new xmlrpcval($forum['auth_read'], 'boolean'),
 			'is_closed'         => new xmlrpcval(TOPIC_LOCKED == $row['topic_status'], 'boolean'),
 			'last_reply_time'   => new xmlrpcval(mobiquo_iso8601_encode($row['topic_time']), 'dateTime.iso8601'),
@@ -98,9 +104,8 @@ function get_topic_func($xmlrpc_params)
 		$topic_list[] = new xmlrpcval($rpc, 'struct');
 	}
 	$db->sql_freeresult($result);
-	if (empty($topic_list)) $topic_list = null;
 
-	$rpc = new xmlrpcval(array(
+	$rpc = array(
 		'total_topic_num' => new xmlrpcval($forum['forum_topics'], 'int'),
 		'forum_id'        => new xmlrpcval($forum['forum_id'], 'string'),
 		'forum_name'      => new xmlrpcval(html_entity_decode($forum['forum_name']), 'base64'),
@@ -108,10 +113,11 @@ function get_topic_func($xmlrpc_params)
 		'unread_sticky_count'   => new xmlrpcval($unread_sticky_num, 'int'),
 		'unread_announce_count' => new xmlrpcval($unread_announce_count, 'int'),
 		'can_subscribe'   => new xmlrpcval($forum['auth_read'], 'boolean'),
-		'is_subscribed'   => new xmlrpcval(!empty($_SESSION['CPG_SESS']['Forums']['track_forums'][$forum['forum_id']]), 'boolean'),
+		'is_subscribed'   => new xmlrpcval($forum['auth_read'] && !empty($_SESSION['CPG_SESS']['Forums']['track_forums'][$forum['forum_id']]), 'boolean'),
+		'require_prefix'  => new xmlrpcval(false, 'boolean'),
 		'prefixes'        => new xmlrpcval(array(), 'array'),
-		'topics'          => new xmlrpcval($topic_list, 'array')
-		), 'struct');
+	);
+	if ($topic_list) $rpc['topics'] = new xmlrpcval($topic_list, 'array');
 
-	return new xmlrpcresp($rpc);
+	return new xmlrpcresp(new xmlrpcval($rpc, 'struct'));
 }
